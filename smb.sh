5e80102ac5af094647c11699933ddbe9f1967f3a
root <root@orion>
2024-01-10 02:47:19 +0800

    update

--- smb.sh
+++ smb.sh
@@ -1,5 +1,6 @@
 apt install samba -y 
 mkdir /smb
 echo -e "\n[SMB文件服务器]\n   path = /smb\n   read only = no" | sudo tee -a /etc/samba/smb.conf
+clear
 echo '请创建系统用户，再通过smbpasswd -a创建smb用户'
 rm -rf /file/smb
